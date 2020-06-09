013b1a5eaa227b9df9f96a04b744b238c08da048
Dahveyea <dahveyea.cowan@gmail.com>
2020-06-08 18:34:19 -0700

    Added API and GET request

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+let request = require('request');
 
+let apiKey = '*****************************';
+let city = 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${'Portland'}&appid=${'a3ea64870e39766f2692cb3145676252'}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    console.log('body:', body);
+  }
+});
